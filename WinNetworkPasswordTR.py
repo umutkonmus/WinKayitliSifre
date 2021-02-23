79e134ef017654841e520aacb84daafb81661034
Umut Konmuş <77108177+umutkonmus@users.noreply.github.com>
2021-02-23 19:31:27 +0300

    Add files via upload

---
+++ WinNetworkPasswordTR.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+ 
+HedefAg = str(subprocess.check_output(["netsh","wlan","show","profiles"]),"cp437").split("\n")
+ 
+def FindNetwork():
+    for i in HedefAg:
+        if "All User Profile" in i:
+            i = i.split(":")
+            yield i[-1].strip()
+ 
+Aglar = list(FindNetwork())
+print(Aglar)
+ 
+AgSec = input("Please type target network name :").strip()
+if AgSec in Aglar:
+    HefefBilgi = str(subprocess.check_output(["netsh","wlan","show","profiles",SelectNetwork,"key=clear"]),"cp437").split("\n")
+    for p in HefefBilgi:
+        if "Key Content" in p:
+            p = p.strip().split(":")
+            print("\nPassword :",p[-1].strip())
+            break
+else:
+    print("Böyle bir ağ bulunamadı !")
+    time.sleep(3)
